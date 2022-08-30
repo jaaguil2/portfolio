91abe601374daf2811ca57be8050e9a648875aee
Joshua Aguilar <joshua.a.aguilar@gmail.com>
2022-08-29 21:12:40 -0400

    nav component done

---
+++ src/app/common/nav/interfaces/nav.interface.ts
@@ -0,0 +1,5 @@
+export interface Link {
+  name: string;
+  route: string;
+  icon: string;
+}
--- src/app/common/nav/nav.component.ts
+++ src/app/common/nav/nav.component.ts
@@ -1,19 +1,42 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
+import { Link } from './interfaces/nav.interface';
 
 @Component({
   standalone: true,
   selector: 'app-nav',
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, MatListModule, MatIconModule],
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.scss']
+  styleUrls: ['./nav.component.scss'],
 })
 export class NavComponent implements OnInit {
+  links: Link[] = [
+    {
+      name: 'Home',
+      route: '/home',
+      icon: 'home',
+    },
+    {
+      name: 'About Me',
+      route: '/about-me',
+      icon: 'account_box',
+    },
+    {
+      name: 'Projects',
+      route: '/projects',
+      icon: 'perm_media',
+    },
+    {
+      name: 'Contact Me',
+      route: '/contact-me',
+      icon: 'alternate_email',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
