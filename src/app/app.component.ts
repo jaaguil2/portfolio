7e62a10b875496af03565ef4b5aac75e267f7348
Joshua Aguilar <joshua.a.aguilar@gmail.com>
2022-08-29 21:23:39 -0400

    code refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { RouterModule } from '@angular/router';
 import { NavComponent } from './common/nav/nav.component';
@@ -9,10 +9,6 @@ import { NavComponent } from './common/nav/nav.component';
   selector: 'app-root',
   imports: [CommonModule, RouterModule, MatSidenavModule, NavComponent],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
-  constructor() { }
-  ngOnInit(): void {
-  }
-}
+export class AppComponent {}
