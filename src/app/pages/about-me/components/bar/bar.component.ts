b07bf5d46d2f1fe25c9b4580e88d160e2bac9bd8
Joshua Aguilar <joshua.a.aguilar@gmail.com>
2022-09-08 08:25:56 -0400

    bar component made

--- src/app/pages/about-me/components/bar/bar.component.ts
+++ src/app/pages/about-me/components/bar/bar.component.ts
@@ -1,15 +1,22 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { Skill } from '../../models/about-me.interface';
 
 @Component({
   standalone: true,
   selector: 'about-me-bar',
-  imports: [CommonModule],
+  imports: [CommonModule, MatProgressBarModule],
   templateUrl: './bar.component.html',
   styleUrls: ['./bar.component.scss'],
 })
-export class BarComponent implements OnInit {
-  constructor() {}
+export class BarComponent {
+  @Input() list!: Skill[];
 
-  ngOnInit(): void {}
+  MAXVALUE = 5;
+  PERCENT = 100;
+
+  valueToPercent(value: number): number {
+    return (this.PERCENT / this.MAXVALUE) * value;
+  }
 }
