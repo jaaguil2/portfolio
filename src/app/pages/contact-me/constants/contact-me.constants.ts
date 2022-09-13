04c91d53a149eb3580efc1992ccf5ea41d70bed7
Joshua Aguilar <joshua.a.aguilar@gmail.com>
2022-09-13 08:32:18 -0400

    updated constants with isLink

--- src/app/pages/contact-me/constants/contact-me.constants.ts
+++ src/app/pages/contact-me/constants/contact-me.constants.ts
@@ -1,7 +1,11 @@
 import { Contact } from '../models/contact-me.interface';
 
 export const CONTACTS: Contact[] = [
-  { name: 'Github', link: 'https://github.com/jaaguil2/' },
-  { name: 'LinkedIn', link: 'https://www.linkedin.com/in/joshua-a-aguilar/' },
-  { name: 'Email', link: 'joshua.a.aguilar@gmail.com' },
+  { name: 'Github', link: 'https://github.com/jaaguil2/', isLink: true },
+  {
+    name: 'LinkedIn',
+    link: 'https://www.linkedin.com/in/joshua-a-aguilar/',
+    isLink: true,
+  },
+  { name: 'Email', link: 'joshua.a.aguilar@gmail.com', isLink: false },
 ];
--- src/app/pages/contact-me/models/contact-me.interface.ts
+++ src/app/pages/contact-me/models/contact-me.interface.ts
@@ -1,4 +1,5 @@
 export interface Contact {
   name: string;
   link: string;
+  isLink: boolean;
 }
