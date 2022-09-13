9c94efbce2bb8a7dc6448153c2c5fd20c9c2156f
Joshua Aguilar <joshua.a.aguilar@gmail.com>
2022-09-13 07:33:19 -0400

    updated links

--- src/app/pages/contact-me/constants/contact-me.constants.ts
+++ src/app/pages/contact-me/constants/contact-me.constants.ts
@@ -1,7 +1,7 @@
 import { Contact } from '../models/contact-me.interface';
 
 export const CONTACTS: Contact[] = [
-  { name: 'Github', link: 'https://github.com/jaaguil2' },
-  { name: 'LinkedIn', link: 'https://www.linkedin.com/in/joshua-a-aguilar' },
+  { name: 'Github', link: 'https://github.com/jaaguil2/' },
+  { name: 'LinkedIn', link: 'https://www.linkedin.com/in/joshua-a-aguilar/' },
   { name: 'Email', link: 'joshua.a.aguilar@gmail.com' },
 ];
