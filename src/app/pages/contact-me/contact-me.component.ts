ed8a733f14da25346f76505eb5ae34fb938b476f
Joshua Aguilar <joshua.a.aguilar@gmail.com>
2022-09-15 08:31:10 -0400

    added cliboard, tooltip, snackbar

--- src/app/pages/contact-me/contact-me.component.ts
+++ src/app/pages/contact-me/contact-me.component.ts
@@ -1,18 +1,40 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { CONTACTS } from './constants/contact-me.constants';
+import { Contact } from './models/contact-me.interface';
+import { Clipboard } from '@angular/cdk/clipboard';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @Component({
   standalone: true,
   selector: 'app-contact-me',
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    MatTooltipModule,
+  ],
   templateUrl: './contact-me.component.html',
-  styleUrls: ['./contact-me.component.scss']
+  styleUrls: ['./contact-me.component.scss'],
 })
-export class ContactMeComponent implements OnInit {
+export class ContactMeComponent {
+  contacts: Contact[] = CONTACTS;
 
-  constructor() { }
+  constructor(private clipboard: Clipboard, private _snackBar: MatSnackBar) {}
 
-  ngOnInit(): void {
+  copyLinkToClipboard(text: string): void {
+    this._snackBar.open('Copied!', 'Close', {
+      duration: 2000,
+    });
+    this.clipboard.copy(text);
   }
-
 }
