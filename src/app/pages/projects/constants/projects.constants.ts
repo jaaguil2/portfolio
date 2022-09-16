f61e806fe7377460a8a873ee8d059a10d4ad617c
Joshua Aguilar <joshua.a.aguilar@gmail.com>
2022-09-16 08:04:32 -0400

    added projects data

---
+++ src/app/pages/projects/constants/projects.constants.ts
@@ -0,0 +1,25 @@
+import { Project } from '../models/projects.models';
+
+export const PROJECTS: Project[] = [
+  {
+    name: 'Portfolio',
+    link: 'https://jaaguil2.github.io/portfolio/home',
+    githubLink: 'https://github.com/jaaguil2/portfolio',
+    image: './assets/images/portfolio.png',
+    notes: "I made this project using Angular 14's standalone components.",
+  },
+  {
+    name: 'DoggoDex',
+    link: 'https://intense-meadow-15815.herokuapp.com/',
+    githubLink: 'https://github.com/jaaguil2/DoggoDex',
+    image: './assets/images/doggo-dex.png',
+    notes: 'A React app I made while still learning.',
+  },
+  {
+    name: 'MyMind',
+    link: 'https://mymind-fe.herokuapp.com/signin',
+    githubLink: 'https://github.com/jaaguil2/MyMind',
+    image: './assets/images/my-mind.png',
+    notes: 'An Angular app I made while still learning.',
+  },
+];
