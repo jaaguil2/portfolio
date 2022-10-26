b8cc614d5a711c61c16de2cea273a0cbf9aeb631
Joshua Aguilar <joshua.a.aguilar@gmail.com>
2022-10-26 18:54:41 -0400

    simples changes made for new landscape view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { RouterModule } from '@angular/router';
 import { LayoutService } from './services/layout.service';
 import { NavComponent } from './shared/components/nav/nav.component';
-import { breakpoints, Layout, breakpointMap } from './models/layout.model';
+import { Layout } from './models/layout.model';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { LayoutType } from './models/app.model';
 
